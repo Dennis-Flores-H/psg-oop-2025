d2eda573d1a2dde90dc124cd7344aafbcb0413b7
Dennis Flores <dfloresh2@fcpn.edu.bo>
2025-12-20 22:08:30 -0400

    Sesion_11

---
+++ sesion11/juego/clases/computadora.py
@@ -0,0 +1,11 @@
+from constantes import OPCIONES
+from random import choice
+class Computadora:
+    def __init__(self):
+        self.nombre = "CPU"
+
+    def __str__(self):
+        return self.nombre
+
+    def elegir(self):
+        return choice(OPCIONES)
